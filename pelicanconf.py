74fccbdd695da4c53aa01a7f9be77ce201b24c32
Dillon Brout <djbrout@gmail.com>
2020-12-17 13:09:13 -0500

    disqus

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,7 +33,7 @@ DEFAULT_PAGINATION = 10
 THEME = "/Users/djbrout/Dropbox/astroapps/ghpages/themes/pelican-themes/aboutwilson"
 
 PLUGIN_PATHS = ["plugins", "/Users/djbrout/Dropbox/astroapps/ghpages/pelican-plugins"]
-PLUGINS = ["assets", "liquid_tags", "sitemap", "pelican_comment_system"]
+PLUGINS = []
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
