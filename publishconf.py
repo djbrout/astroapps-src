85542cc6006c8e7c905e71dd9dd67b7ff56d29d3
Dillon Brout <djbrout@gmail.com>
2020-12-21 11:29:53 -0500

    astro-apps disqus

--- publishconf.py
+++ publishconf.py
@@ -21,5 +21,5 @@ DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "astroapps"
+DISQUS_SITENAME = "astro-apps"
 GOOGLE_ANALYTICS = "G-EBFVZDB383"
